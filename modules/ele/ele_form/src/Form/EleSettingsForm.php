ba9d8df2b15f7a6e2fe5a61f2447028bac22a631
Elyzium <guangzhiwoxin@gmail.com>
2017-05-30 23:24:59 +0800

    global config fields definition and output in twig template , about and contact page template

--- modules/ele/ele_form/src/Form/EleSettingsForm.php
+++ modules/ele/ele_form/src/Form/EleSettingsForm.php
@@ -32,17 +32,167 @@ class EleSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
     $config = $this->config('ele_form.settings');
 
-    $form['home_first_title'] = array(
+    $form['first_screen'] = [
+      '#type' => 'details',
+      '#title' => t('Home Description on first screen'),
+      '#open'  => TRUE,
+    ];
+
+    $form['first_screen']['home_first_title'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Branding'),
       '#default_value' => $config->get('home_first_title'),
     );
 
-    $form['home_first_desc'] = array(
+    $form['first_screen']['home_first_desc'] = array(
         '#type' => 'textarea',
         '#title' => $this->t('Branding description'),
         '#default_value' => $config->get('home_first_desc'),
      ); 
+
+    $form['first_screen']['home_second_title'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Photography'),
+      '#default_value' => $config->get('home_second_title'),
+    );
+
+    $form['first_screen']['home_second_desc'] = array(
+        '#type' => 'textarea',
+        '#title' => $this->t('Photography description'),
+        '#default_value' => $config->get('home_second_desc'),
+     ); 
+
+    $form['first_screen']['home_third_title'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Web design'),
+      '#default_value' => $config->get('home_third_title'),
+    );
+
+    $form['first_screen']['home_third_desc'] = array(
+        '#type' => 'textarea',
+        '#title' => $this->t('Web design description'),
+        '#default_value' => $config->get('home_third_desc'),
+     ); 
+
+
+    $form['second_screen'] = [
+      '#type' => 'details',
+      '#title' => t('Home description on second screen'),
+      '#open'  => FALSE,
+    ];
+
+    $form['second_screen']['home_second_what'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('what'),
+      '#default_value' => $config->get('home_second_what'),
+    );
+
+    $form['second_screen']['home_second_what_desc'] = array(
+        '#type' => 'textarea',
+        '#title' => $this->t('What description'),
+        '#default_value' => $config->get('home_second_what_desc'),
+     ); 
+
+    $form['second_screen']['home_second_who'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('who'),
+      '#default_value' => $config->get('home_second_who'),
+    );
+    $form['second_screen']['home_second_who_desc'] = array(
+        '#type' => 'textarea',
+        '#title' => $this->t('Who description'),
+        '#default_value' => $config->get('home_second_who_desc'),
+     ); 
+
+    $form['second_screen']['home_second_how'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('how'),
+      '#default_value' => $config->get('home_second_how'),
+    );
+    $form['second_screen']['home_second_how_desc'] = array(
+        '#type' => 'textarea',
+        '#title' => $this->t('How description'),
+        '#default_value' => $config->get('home_second_how_desc'),
+     ); 
+
+
+    $form['third_screen'] = [
+      '#type' => 'details',
+      '#title' => t('Hoem description on third screen'),
+      '#open'  => FALSE,
+    ];
+
+    $form['third_screen']['home_third_small_title'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Small title on picture'),
+      '#default_value' => $config->get('home_third_small_title'),
+    );
+
+    $form['third_screen']['home_third_strong_title'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Strong title on picture'),
+        '#default_value' => $config->get('home_third_strong_title'),
+     ); 
+
+    $form['home_work_section'] = [
+      '#type' => 'details',
+      '#title' => t('Home work section'),
+      '#open'  => FALSE,
+    ];
+
+    $form['home_work_section']['home_work_section_title'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Work section title'),
+      '#default_value' => $config->get('home_work_section_title'),
+    );
+
+    $form['home_work_section']['home_work_section_desc'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Work section description'),
+        '#default_value' => $config->get('home_work_section_desc'),
+     ); 
+
+    $form['home_contact_section'] = [
+      '#type' => 'details',
+      '#title' => t('Home contact section'),
+      '#open'  => FALSE,
+    ];
+
+    $form['home_contact_section']['home_contact_section_title'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Home contact section title'),
+      '#default_value' => $config->get('home_contact_section_title'),
+    );
+
+    $form['home_contact_section']['home_contact_section_desc'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Home contact section description'),
+        '#default_value' => $config->get('home_contact_section_desc'),
+     ); 
+
+    $form['site_footer'] = [
+      '#type' => 'details',
+      '#title' => t('Elements on footer'),
+      '#open'  => FALSE,
+    ];
+
+    $form['site_footer']['site_footer_email'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Email'),
+      '#default_value' => $config->get('site_footer_email'),
+    );
+
+    $form['site_footer']['site_footer_phone'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Phome'),
+      '#default_value' => $config->get('site_footer_phone'),
+    );
+
+    $form['site_footer']['site_footer_address'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Address'),
+      '#default_value' => $config->get('site_footer_address'),
+    );
 
     return parent::buildForm($form, $form_state);
   }
@@ -55,7 +205,26 @@ class EleSettingsForm extends ConfigFormBase {
     $this->config('ele_form.settings')
       ->set('home_first_title', $values['home_first_title'])
       ->set('home_first_desc', $values['home_first_desc'])
-      ->save();
+      ->set('home_second_title', $values['home_second_title'])
+      ->set('home_second_desc', $values['home_second_desc'])
+      ->set('home_third_title', $values['home_third_title'])
+      ->set('home_third_desc', $values['home_third_desc'])
+      ->set('home_second_what', $values['home_second_what'])
+      ->set('home_second_what_desc', $values['home_second_what_desc'])
+      ->set('home_second_who', $values['home_second_who'])
+      ->set('home_second_who_desc', $values['home_second_who_desc'])
+      ->set('home_second_how', $values['home_second_how'])
+      ->set('home_second_how_desc', $values['home_second_how_desc'])
+      ->set('home_third_small_title', $values['home_third_small_title'])
+      ->set('home_third_strong_title', $values['home_third_strong_title'])
+      ->set('home_work_section_title', $values['home_work_section_title'])
+      ->set('home_work_section_desc', $values['home_work_section_desc'])
+      ->set('home_contact_section_title', $values['home_contact_section_title'])
+      ->set('home_contact_section_desc', $values['home_contact_section_desc'])
+      ->set('site_footer_email', $values['site_footer_email'])
+      ->set('site_footer_phone', $values['site_footer_phone'])
+      ->set('site_footer_address', $values['site_footer_address'])
+   ->save();
   }
 
 }
